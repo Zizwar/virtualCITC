6399b9c785b1ddf68fcd2b5a7445ce1467fd131f
ibrahimbidi <64697249+ibrahimbidi@users.noreply.github.com>
2020-06-01 06:12:40 +0300

    Update citc.ts

--- services/citc.ts
+++ services/citc.ts
@@ -16,11 +16,11 @@ export const orderService = async ({
     request: Request;
 }) => {
     
-    const { id } = params;
+    const { id:typeOrder } = params;
 
     let body:any = await request.body();
     body = body.value;
-    const refernceCode = id!=="cancel" ?Math.random().toString(36).substring(2):"cancel";
+    const refernceCode = typeOrder!=="cancel" ?Math.random().toString(36).substring(2):"cancel";
   
     const data = {refernceCode,body};
     console.log({data});
@@ -28,7 +28,7 @@ export const orderService = async ({
     //const insertOrder = await orderDB.insertOne(data);
     //console.log('insertDB',{insertOrder})
     const status = true;
-    response.body = {data,status};
+    response.body = {typeOrder,status,data};
     response.status = 200;
 
 };
@@ -42,13 +42,14 @@ export const driverService = async ({
     response: Response;
     request: Request;
 }) => {
-    const { id:typeOrder } = params;
-    const body:any = await request.body();
+    const { id:typeDriver } = params;
+    let body:any = await request.body();
+    body = body.value;
     const refernceCode = Math.random().toString(36).substring(2);
-    const data = [...body,{refernceCode}];
+    const data = {refernceCode,body};
     console.log({data});
     const status = true;
-    response.body = {data,status};
+    response.body = {typeDriver,status,data};
     response.status = 200;
 
 };
